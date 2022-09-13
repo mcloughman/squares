1515e9e6adc7019825dc84d17b4db22f0c938381
Michael Loughman <mcloughman@gmail.com>
2022-09-12 19:53:09 -0700

    remove bg from css and assign it dynamically in index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const containerGrid = document.querySelector(".container")
+
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+
 let counter = 0;
 for (let i = 0; i < 6; i++) {
     for (let j = 0; j < 6; j++) {
@@ -6,6 +15,9 @@ for (let i = 0; i < 6; i++) {
         newDiv.classList.add("square")
         newDiv.setAttribute("data-id", counter);
         newDiv.innerHTML = counter;
+        const intervalId = setInterval(() => {
+            newDiv.style.backgroundColor = randomRGB()
+        }, 2000)
         counter++;
         containerGrid.append(newDiv)
     }
